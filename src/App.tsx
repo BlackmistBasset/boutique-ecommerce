c6e95851f9583a130ad7c6c4746f7beab252cfb2
Basset <aldana.sorni@gmail.com>
2024-06-18 00:30:26 -0300

    finished products appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import { Products } from './pages/Products';
+import { Login } from './pages/Login';
+import { Cart } from './pages/Cart';
+
+import { Typography } from '@mui/material';
 export const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <h1>Boutique</h1>
+        <Typography variant="h5" sx={{ marginBlock: '30px' }}>
+          Boutique avaiable products
+        </Typography>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>*/}
       </div>
-    </>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
---
+++ src/types/products.ts
@@ -0,0 +1,8 @@
+export interface ProductType {
+  id: string;
+  image: string;
+  price: number;
+  stock: number;
+  name: string;
+  description: string;
+}
