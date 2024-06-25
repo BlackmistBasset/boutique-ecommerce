41bb7a46de8c3c76917c838596250f727e38e3db
Basset <aldana.sorni@gmail.com>
2024-06-25 17:24:13 -0300

    protected routes, user stored on local storage, login complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { Products } from './pages/Products';
 import { Login } from './pages/Login';
@@ -9,22 +8,22 @@ import { Orders } from './pages/Orders';
 import { Navbar } from './components/Navbar';
 
 import { CssBaseline } from '@mui/material';
+import { useAuth } from './context/AuthContext';
+
 export const App = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <CssBaseline />
+      <CssBaseline />
+      <Navbar />
 
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products" element={isAuthenticated ? <Products /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/products" />} />
+        <Route path="/cart" element={isAuthenticated ? <Cart /> : <Navigate to="/login" />} />
+        <Route path="/orders" element={isAuthenticated ? <Orders /> : <Navigate to="/login" />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import { AuthContextType, UserType } from '../types/auth';
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -6,14 +6,25 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<UserType | null>(null);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      setUser(parsedUser);
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   const login = (user: UserType) => {
     setUser(user);
     setIsAuthenticated(true);
+    localStorage.setItem('user', JSON.stringify(user));
   };
 
   const logout = () => {
     setUser(null);
     setIsAuthenticated(false);
+    localStorage.removeItem('user');
   };
 
   return <AuthContext.Provider value={{ isAuthenticated, login, logout, user }}>{children}</AuthContext.Provider>;
